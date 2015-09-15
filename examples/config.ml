3ab25d74efeb0836b40af56045475180514208c9
Magnus Skjegstad <magnus@skjegstad.com>
2015-09-15 13:46:05 +0100

    Update package name in example

--- examples/config.ml
+++ examples/config.ml
@@ -4,7 +4,7 @@ let main = foreign "Unikernel.Main" (console @-> job)
 
 let () =
   add_to_ocamlfind_libraries [ "mirage-bootvar" ; "lwt" ; "re" ];
-  add_to_opam_packages [ "mirage-bootvar" ; "lwt" ; "re" ];
+  add_to_opam_packages [ "mirage-bootvar-xen" ; "lwt" ; "re" ];
 
   register "unikernel" [
     main $ default_console
