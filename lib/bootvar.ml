3a2cd5fbf877b728439acc8752938dde07250b1b
Martin Lucina <martin@lucina.net>
2020-06-18 09:58:52 +0200

    Realign interface with mirage-bootvar-solo5
    The additional processing done to support the existing Xen-specific
    Bootvar interfaces seems to be interfering with the new PVH-only Xen
    stack; command line arguments passed via xl.cfg cmdline= are getting
    dropped on the floor.
    
    Since the Xen-specific interfaces don't appear to be used anywhere, and
    mirage/mirage can be trivially updated to use only Bootvar.argv()
    without filtering, this change drops the Xen-specific interfaces
    altogether, effectively aligning the interface with that provided by
    mirage-bootvar-solo5, with the sole difference being dependencies on the
    Xen stack, and the naming of the external call used to get the raw
    command line.

--- lib/bootvar.ml
+++ lib/bootvar.ml
@@ -14,60 +14,10 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  *
  *)
-open Lwt
-
-type t = { cmd_line : string;
-           parameters : (string * string) list }
-
-exception Parameter_not_found of string
-
 external get_cmd_line : unit -> string = "mirage_xen_get_cmdline"
 
-let create () =
-  let cmd_line_raw = get_cmd_line () in
-  (* Strip leading whitespace *)
-  let filter_map fn l =
-    List.fold_left (fun acc x ->
-        match fn x with Some y -> y::acc | None -> acc) [] l
-  in
-  let entries = Parse_argv.parse cmd_line_raw in
-  match entries with
-  | Error s -> fail_with s
-  | Ok l ->
-    let parameters =
-      filter_map (fun x ->
-        match Astring.String.cut ~sep:"=" x with
-        | Some (a,b) ->
-          Some (a,b)
-        | _ ->
-          Printf.printf "Ignoring malformed parameter: %s\n" x; None
-      ) l
-    in
-    Lwt.return { cmd_line=cmd_line_raw ; parameters}
-
-let get_exn t parameter =
-  try
-    List.assoc parameter t.parameters
-  with
-    Not_found -> raise (Parameter_not_found parameter)
-
-let get t parameter =
-  try
-    Some (List.assoc parameter t.parameters)
-  with
-    Not_found -> None
-
-let parameters x = x.parameters
-
-let to_argv ~filter x =
-  let x = List.filter filter x in
-  let argv = Array.make (1 + List.length x) "" in
-  let f i (k,v) =
-    let dash = if String.length k = 1 then "-" else "--" in
-    argv.(i + 1) <- Printf.sprintf "%s%s=%s" dash k v
-  in
-  List.iteri f x ;
-  argv
-
-let argv ?(filter=(fun _ -> true)) () =
-  create () >>= fun t -> return (to_argv ~filter @@ parameters t)
+let argv () =
+  let cmd_line = get_cmd_line () in
+  match Parse_argv.parse cmd_line with
+  | Ok l -> Lwt.return (Array.of_list ("mirage" :: l))
+  | Error s -> Lwt.fail_with s
